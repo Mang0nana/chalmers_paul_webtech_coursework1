ede8b5bbf504a89ba1cfc75a733a8a60f09727e1
Mang0nana <46711976+Mang0nana@users.noreply.github.com>
2019-03-06 21:46:15 +0000

    Add files via upload

---
+++ coursework/diffie_hellman.js
@@ -0,0 +1,21 @@
+function difhell()
+{
+	
+	var g_value = document.getElementById("g").value;
+	var n_value = document.getElementById("n").value;
+	var x_value = document.getElementById("x").value;
+	var y_value = document.getElementById("y").value;
+
+	var bob_calc1 = (g_value**x_value) % n_value;
+	document.getElementById("bob1").innerHTML = bob_calc1;
+
+	var alice_calc1 = (g_value**y_value) % n_value;
+	document.getElementById("alice1").innerHTML = alice_calc1
+
+	var bob_calc2 = (alice_calc1**=x_value) % n_value;
+	document.getElementById("bob2").innerHTML = bob_calc2;
+	
+	var alice_calc2 = (bob_calc1**=y_value) % n_value;
+	document.getElementById("alice2").innerHTML = alice_calc2;
+	alert(bob_calc2)
+}
---
+++ coursework/rot.js
@@ -0,0 +1,70 @@
+// http://stackoverflow.com/a/617685/987044
+//function rot(s, i) {
+//			return s.replace(/[a-zA-Z]/g, function (c) {
+//				return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + i) ? c : c - 26);
+//			});
+//		}
+//		function update() {
+//			document.getElementById('output').value = rot(document.getElementById('input').value, +document.getElementById('rot').value);
+//		}
+
+function encrypt()
+{
+  var plain_text = document.getElementById("message").value;
+  var shift = document.getElementById('cshift').value|0;
+  var cypher_text = [];
+  var alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  
+  for (idx=0; idx<plain_text.length; idx++)
+  {
+    input = alphabet.indexOf(plain_text[idx]);
+    if(input == -1)
+    {
+      cypher_text.push(plain_text[idx]);
+    }
+  
+    else
+    {
+      var coded = (input+shift)%26;
+      var letter = alphabet[coded];
+      cypher_text.push(letter);
+
+    }
+  }
+  document.getElementById("eoutput").innerHTML = cypher_text.join("");
+
+}
+
+
+
+
+function decrypt()
+{
+var plain_text = document.getElementById("ciphert").value;
+var shift = document.getElementById('dcshift').value|0;
+var cypher_text = [];
+var alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];  
+  
+  for (idx=0; idx<plain_text.length; idx++)
+  {
+    input = alphabet.indexOf(plain_text[idx]);
+    if(input == -1)
+    {
+      cypher_text.push(plain_text[idx]);
+    }
+  
+    else
+    {
+      var dec = (26-shift);
+      var coded = (input+dec)%26;
+      var letter = alphabet[coded];
+      cypher_text.push(letter);
+
+    }
+  }
+  document.getElementById("doutput").innerHTML = cypher_text.join("");
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
